2ac3683aad7ec4bf07c106e81ae229754770bd0b
ចិន <chenpork@hotmail.com>
2020-02-27 22:10:08 +0700

    newUpdated

--- students/ChanchenPK/HomeWork1/homePage.php
+++ students/ChanchenPK/HomeWork1/homePage.php
@@ -1,4 +1,28 @@
 <?php
+    $visitor= 100;
+    $like= 20;
+    $dislike= 10;
+    $visitorHistory = array(
+        "row1" => array(
+            'time'=> "Yesterday 3pm", 
+            'visiting_page' => "Home page",
+            'impression' => "Good", 
+            'visiting_device' => 'Chrome'
+        ),
+        "row2" => array(
+            'time'=> "Yesterday 5pm", 
+            'visiting_page' => "Post 1", 
+            'impression' =>"Good", 
+            'visiting_device' => "iPad"
+        ),
+        "row3" => array(
+            "time"=> "Today 7am", 
+            "visiting_page" => "Post 2", 
+            "impression" => null,
+            "visiting_device" => "Firefox"
+        )
+    );
+
     echo"
         <head>
             <title>Home Page</title>
@@ -50,62 +74,72 @@
           tr:nth-child(even) {
             background-color: #dddddd;
           }
-    </style>
+        </style>
+    <body>
 
-    <h2>Home <span style = 'display: inline; border-left-style:solid;'><a href='myPost.php' style='padding-left:5px;'>My Post</a></span></h2>
-    <h1>Welcome to My Blog!</h1>
-    <br>
+        <h2>Home <span style = 'display: inline; border-left-style:solid;'><a href='myPost.php' style='padding-left:5px;'>My Post</a></span></h2>
+        <h1>Welcome to My Blog!</h1>
+        <br>
 
-    <div>
-        <h2 style = 'display: inline-block;'>Total Impression</h2>
-        <select style = 'display: inline-block; margin-left: 300px'>
-            <option value='thisMonth'>This Month</option>
-        </select>
-    </div>
-    <br>
-    <div>
-        <div class='box'>
-            <img src='visitor.png' atl='visitor' style='width:50px; padding-left: 35%;'>
-                <br><span style = 'padding-left: 25%'>390 Visitors</span>
+        <div>
+            <h2 style = 'display: inline-block;'>Total Impression</h2>
+            <select style = 'display: inline-block; margin-left: 300px'>
+                <option value='thisMonth'>This Month</option>
+            </select>
         </div>
-        <div class='box'>
-            <img src='like.png' atl='like' style='width:50px; padding-left: 35%;'>
-                <br><span style = 'padding-left: 28%'>28 likes</span>
+        <br>
+        <div>
+            <div class='box'>
+                <img src='visitor.png' atl='visitor' style='width:50px; padding-left: 35%;'>
+        
+                    <br><span style = 'padding-left: 25%'>$visitor Visitors</span>
+            </div>
+            <div class='box'>
+                <img src='like.png' atl='like' style='width:50px; padding-left: 35%;'>
+                    <br><span style = 'padding-left: 28%'>$like likes</span>
+            </div>
+            <div class='box'>
+                <img src='dislike.png' atl='dislike' style='width:50px; padding-left: 35%;'>
+                    <br><span style = 'padding-left: 26%'>$dislike dislikes</span>
+            </div>
         </div>
-        <div class='box'>
-            <img src='dislike.png' atl='dislike' style='width:50px; padding-left: 35%;'>
-                <br><span style = 'padding-left: 26%'>12 dislikes</span>
-        </div>
-    </div>
-    <br>
-    <h2>Visitor's history</h2>
-    <br>
-    <table>
-        <tr>
-            <th>Time</th>
-            <th>Visit page</th>
-            <th>Impression</th>
-            <th>Visiting device</th>
-        </tr>
-        <tr>
-            <td>Yesterday 3pm</td>
-            <td>Home page</td>
-            <td>Good</td>
-            <td>Chrome</td>  
-        </tr>
-        <tr>
-            <td>Yesterday 5pm</td>
-            <td>Post1</td>
-            <td>Good</td>
-            <td>Ipad</td>  
-        </tr>
-        <tr>
-            <td>Today 7pm</td>
-            <td>Post2</td>
-            <td></td>
-            <td>Fire Fox</td>  
-        </tr>
-    </table>
+        <br>
+        <h2>Visitor's history</h2>
+        <br>
+        <table>
+            <tr>
+                <th>Time</th>
+                <th>Visit page</th>
+                <th>Impression</th>
+                <th>Visiting device</th>
+            </tr>
+            <tr>
+        ";
+                echo "<td>".$visitorHistory['row1']['time']."</td>";
+                echo "<td>".$visitorHistory['row1']['visiting_page']."</td>";
+                echo "<td>".$visitorHistory['row1']['impression']."</td>";
+                echo "<td>".$visitorHistory['row1']['visiting_device']."</td>";
+                
+        echo"
+            </tr>
+            <tr>
+            ";
+            echo "<td>".$visitorHistory['row2']['time']."</td>";
+            echo "<td>".$visitorHistory['row2']['visiting_page']."</td>";
+            echo "<td>".$visitorHistory['row2']['impression']."</td>";
+            echo "<td>".$visitorHistory['row2']['visiting_device']."</td>";
+        echo"
+            </tr>
+            <tr>
+            ";
+            echo "<td>".$visitorHistory['row3']['time']."</td>";
+            echo "<td>".$visitorHistory['row3']['visiting_page']."</td>";
+            echo "<td>".$visitorHistory['row3']['impression']."</td>";
+            echo "<td>".$visitorHistory['row3']['visiting_device']."</td>";
+        echo"
+            </tr>
+        </table>
+    </body>
     
     "
 
