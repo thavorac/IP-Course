448e03debbff6c3f5a1439fc7c9812029a58db43
Naroth24 <kunvannaroth@gmail.com>
2020-02-28 08:58:06 +0700

    variable update

--- students/Korn Vannaroth/Mypost.php
+++ students/Korn Vannaroth/Mypost.php
@@ -26,20 +26,20 @@ echo "
 </p><br>
 <div class='overflow'>
     <img src='user.jpg' alt=''>A user is a person who utilizes a computer or network service. Users of computer systems and software products generally lack the technical expertise required to fully understand how they work.
-    <br><span style='float: right;'>Saved 3hours ago</span>
+    <br><br><br><br><br><span style='float: right;'>Saved 3hours ago</span>
 </div><br>
 <div class='overflow'>
     <img src='apple.jpg' alt=''>Apple Inc. is an American multinational technology company headquartered in Cupertino, California, that designs, develops, and sells consumer electronics, computer software, and online services. It is considered one of the Big Four technology companies, alongside Amazon, Google, and Facebook.
-    <br><span style='float: right;'>Saved yesterday 9pm ago</span>
+    <br><br><br><span style='float: right;'>Saved yesterday 9pm ago</span>
 </div><br>
 <h2><a href=''>Public Posts</a></h2><br>
 <div class='overflow'>
     <img src='android.jpg' alt=''>Android is a mobile operating system based on a modified version of the Linux kernel and other open source software, designed primarily for touchscreen mobile devices such as smartphones and tablets
-    <br><span style='float: right;'>Publiced 3hours ago</span>
+    <br><br><br><br><span style='float: right;'>Publiced 3hours ago</span>
 </div><br>
 <div class='overflow'>
     <img src='google.jpg' alt=''>Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware
-    <br><span style='float: right;'>Publiced yesterday 9pm</span>
+    <br><br><br><br><span style='float: right;'>Publiced yesterday 9pm</span>
 </div>
 </body>
 
--- students/Korn Vannaroth/hw1.php
+++ students/Korn Vannaroth/hw1.php
@@ -1,9 +1,22 @@
 <?php
-echo"
+$visitors = 390;
+$likes = 28;
+$dislike = 12;
+$visitorHistories= array(
+    array("time"=>"yesterday 3pm","visiting page"=>"homepage","impression"=>"good","visiting device"=>"chrome"),
+    array("time"=>"yesterday 5pm","visiting page"=>"page 1","impression"=>"good","visiting device"=>"ipone"),
+    array("time"=>"today 7am","visiting page"=>"page 2","impression"=>"","visiting device"=>"firefox"),
+)
+
+?>
+
      <html>
      <style>
      table, th, td {
        border: 1px solid black;
+     }
+     ul{
+         list-style-type : none;
      }
      ul li{
           float: left;
@@ -43,43 +56,39 @@ echo"
         <select name='' id='' style='border: 3px solid black;'>
             <option value=''>This Month</option>
             <option value=''>Last Month</option>
-            <option value=''>This year</option>
-            <option value=''>Last year</option>
+            <option value=''>2 Month ago</option>
+            <option value=''>3 Month ago</option>
         </select>
     </span>
+    <?php
+    echo"
     <div><ul>
-        <li><div><img src='visitor.png' alt= '' style='width: 30px; height: 30px;'><br>390 Visitors</div></li>
-        <li><div><img src='like.jpg' alt='' style='width: 30px; height: 30px;'><br>28 Likes</div></li>
-        <li><div><img src='dislike.png' alt='' style='width: 30px; height: 30px;'><br>12 Dislikes</div></li>
+        <li><div><img src='visitor.png' alt= '' style='width: 30px; height: 30px; padding:7px;'><br>$visitors</div></li>
+        <li><div><img src='like.jpg' alt='' style='width: 30px; height: 30px; padding:7px;'><br>$likes</div></li>
+        <li><div><img src='dislike.png' alt='' style='width: 30px; height: 30px; padding:5px;'><br>$dislike</div></li>
         </ul>
     </div>
+    "
+    ?>
      <br><b>Visitor's history</b></br>
      <table><tr style='border-top: 2px solid black; background-color: darkgrey;'>
-         <td>Time</td>
+        
+        <td>Time</td>
          <td>Visiting page</td>
          <td>Impression</td>
          <td>visiting device</td>
         
     </tr>
-    <tr>
-         <td>Yesterday 3pm</td>
-         <td>Homepage</td>
-         <td>Good</td>
-         <td>Chrome</td>
-    </tr>
-    <tr>
-     <td>Yesterday 5pm</td>
-     <td>page1</td>
-     <td>Good</td>
-     <td>Iphone</td>
-    </tr>
-    <tr>
-    <td>Today 7am</td>
-    <td>page2</td>
-    <td></td>
-    <td>Firefox</td>
-    </tr>
+    <?php
+    foreach($visitorHistories as $visitorHistory)
+      echo"
+        <tr>
+            <td>".$visitorHistory['time']."</td>
+            <td>".$visitorHistory['visiting page']."</td>
+            <td>".$visitorHistory['impression']."</td>
+            <td>".$visitorHistory['visiting device']."</td>
+        </tr>
+        "
+        ?>
      </table>
      </body></html>
-"
-?>
